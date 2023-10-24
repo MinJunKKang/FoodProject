e88facf80a0de3ed315e6fe2e5ff26d178f83515
bbky3 <bbky323@kau.kr>
2023-10-24 09:55:34 +0900

    modify_2

--- app/src/main/java/com/example/myfoodproject/MainActivity.kt
+++ app/src/main/java/com/example/myfoodproject/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.myfoodproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import com.example.myfoodproject.databinding.ActivityMainBinding
+import javax.net.ssl.SSLSessionBindingEvent
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
