3a2f7c158d9752fb82c4db9ba6381f8322db6d55
kavewu57 <bifur03@kau.kr>
2023-10-27 18:06:17 +0900

    git commit1

--- app/src/main/java/com/example/japanese_restaurant/MainActivity.kt
+++ app/src/main/java/com/example/japanese_restaurant/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.japanese_restaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val image_ramen = findViewById<ImageView>(R.id.ramen)
+        image_ramen.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
